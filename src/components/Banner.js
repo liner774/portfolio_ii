ed5ab63016ad71be6f0ee682fc4e025c4d3c044a
Nyi Lynn <54408043+liner774@users.noreply.github.com>
2024-06-23 12:27:54 +0630

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -21,27 +21,27 @@ export const Banner = () => {
     document.body.removeChild(link);
   };
 
-  const tick = useCallback(() => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+ const tick = useCallback(() => {
+  let i = loopNum % toRotate.length;
+  let fullText = toRotate[i];
+  let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
-    setText(updatedText);
+  setText(updatedText);
 
-    if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
-    }
+  if (isDeleting) {
+    setDelta(prevDelta => prevDelta / 2);
+  }
 
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setLoopNum(loopNum + 1);
-      setDelta(period);
-    } else if (isDeleting && updatedText === '') {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setDelta(500);
-    }
-  }, [loopNum, isDeleting, text, delta, period]);
+  if (!isDeleting && updatedText === fullText) {
+    setIsDeleting(true);
+    setLoopNum(loopNum + 1);
+    setDelta(period);
+  } else if (isDeleting && updatedText === '') {
+    setIsDeleting(false);
+    setLoopNum(loopNum + 1);
+    setDelta(500);
+  }
+}, [loopNum, isDeleting, text, toRotate, delta, period]);
 
   useEffect(() => {
     let ticker = setInterval(() => {
