3a6d01f1cfc35d37c122745ccaa411222a6f3eb0
Nyi Lynn <54408043+liner774@users.noreply.github.com>
2024-06-23 12:31:19 +0630

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -8,6 +8,7 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
   const toRotate = ["Web Developer", "Web Designer", "Game Dev", "Artist"];
   const period = 2000;
 
@@ -21,35 +22,38 @@ export const Banner = () => {
     document.body.removeChild(link);
   };
 
- const tick = useCallback(() => {
-  let i = loopNum % toRotate.length;
-  let fullText = toRotate[i];
-  let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-  setText(updatedText);
-
-  if (isDeleting) {
-    setDelta(prevDelta => prevDelta / 2);
-  }
-
-  if (!isDeleting && updatedText === fullText) {
-    setIsDeleting(true);
-    setLoopNum(loopNum + 1);
-    setDelta(period);
-  } else if (isDeleting && updatedText === '') {
-    setIsDeleting(false);
-    setLoopNum(loopNum + 1);
-    setDelta(500);
-  }
-}, [loopNum, isDeleting, text, toRotate, delta, period]);
-
   useEffect(() => {
     let ticker = setInterval(() => {
       tick();
     }, delta);
 
     return () => clearInterval(ticker);
-  }, [tick, delta]);
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  };
 
   return (
     <section className="banner" id="home">
@@ -62,7 +66,7 @@ export const Banner = () => {
                   <span className="tagline">Welcome to my Portfolio</span>
                   <h1>{`Hi! I'm Lynn, `}<br /> <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "Game Dev", "Artist" ]'><span className="wrap">{text}</span></span></h1>
                   <p>
-                    Hey there! I'm <b style={{ color: '#fad374' }}>Nyi Lynn Set</b>, a fresh full stack web developer. I love building websites that users find easy to use.
+                    Hey there! I'm<b style={{ color: '#fad374' }}>Nyi Lynn Set</b>, a fresh full stack web developer. I love building websites that users find easy to use.
                     Throughout my studies, I've focused on user-friendly design and writing clean, efficient code.
                     I enjoy teaming up with others and bringing projects to life.
                     <b style={{ color: '#091b35' }}>Thanks for checking out my portfolio! I'm excited to chat about any opportunities you have in mind!</b>
@@ -85,4 +89,4 @@ export const Banner = () => {
       </Container>
     </section>
   );
-};
+}
