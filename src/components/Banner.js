bb471e5a4e6a20bd2e4c82405ed35f2b53aeea0a
Nyi Lynn <54408043+liner774@users.noreply.github.com>
2024-06-23 10:50:48 +0630

    text bar color

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -65,7 +65,7 @@ export const Banner = () => {
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <span className="tagline">Welcome to my Portfolio</span>
                   <h1>{`Hi! I'm Lynn, `}<br /> <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "Game Dev", "Artist" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>
+                  <p >
                     Hey there! I'm<b style={{ color: '#fad374' }}>Nyi Lynn Set</b>, a fresh full stack web developer. I love building websites that users find easy to use.
                     Throughout my studies, I've focused on user-friendly design and writing clean, efficient code.
                     I enjoy teaming up with others and bringing projects to life.
